1d5aabd47a1220a4b4991ddf2b89f12ce5f71115
Andreas Schueller <apschueller@gmail.com>
2018-06-13 17:53:39 -0400

    Remove path info from outputs as a security measure

--- src/LDM/MainBundle/Resources/bin/dr_sasa.sh
+++ src/LDM/MainBundle/Resources/bin/dr_sasa.sh
@@ -5,10 +5,17 @@
 # <zip_name> - Name of the zip archive without extension
 
 # Print dr_sasa command passed as 1st argument to file (as info)
-echo "$1" >command_line
+# But first we need to strip all path information, for security reasons
+arr=($1) # Split into array
+n='' # Empty string
+for a in "${arr[@]}"; do n="$n ${a##*/}"; done # Loop over array, strip path from each element and join by space to new string
+echo "$n" >command_line
 
 # Run dr_sasa command passed as 1st argument
 eval "$1" >out 2>err
+
+# First line of output contains the full path of the input structure. We need to strip the path
+sed -i '' '1s:.*/::' out
 
 # Create zip archive, name provided as 2nd argument
 list=$(ls)
