c7ba7f7b5c14d216b8f58d8f295945afdfc03c7a
Andreas Schuller <aschueller@uc.cl>
2023-04-03 11:21:16 -0400

    Added comments

--- app/build_bootstrap.sh
+++ app/build_bootstrap.sh
@@ -1,1 +1,3 @@
+# Build bootstrap cache on fresh install
+# Source: https://stackoverflow.com/a/35085409
 php ../vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php
